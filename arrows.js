163364f62904b037d532e02d07721289b8513765
zburstein <zburstein@gmail.com>
2017-03-30 15:51:54 -0700

    add arrow funciton, format ss

---
+++ arrows.js
@@ -0,0 +1,14 @@
+$(document).keydown(function(e) {
+  //blcok default (scroll)
+
+  //left arrow
+  if(e.which == 37){
+    var newURL = document.getElementById('leftArrow').href;
+    window.location.href = newURL;
+  }
+  //right arrow
+  else if(e.which == 39){
+    var newURL = document.getElementById('rightArrow').href;
+    window.location.href = newURL;
+  }
+});
--- swapImage.js
+++ swapImage.js
@@ -1,4 +1,4 @@
 /*change the product image*/
 function swapImage(newImage){
-	document.getElementById('mainImage').src = newImage.src;
+	document.getElementById('mainImage').src = document.getElementById(newImage).src;
 }
